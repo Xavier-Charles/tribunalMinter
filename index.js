caa51bb1485482b11f626ade4f39396249ef25b7
Xharles <charlesnzubechukwu000@gmail.com>
2022-04-05 07:36:25 +0100

    add cors

--- index.js
+++ index.js
@@ -5,6 +5,31 @@ const { Mint } = require("./scripts/mint-nft");
 // Initialize Express
 const app = express();
 
+// Use this soon
+// var allowedOrigins = ["http://localhost:3000", "http://yourapp.com"];
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         var msg =
+//           "The CORS policy for this site does not " +
+//           "allow access from the specified Origin.";
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
+
+
+app.use(cors())
+
+//Enable prefligt
+app.options('*', cors()) // include before other routes
+
 // Parse JSON bodies for this app. Make sure you put
 // `app.use(express.json())` **before** your route handlers!
 app.use(express.json());
