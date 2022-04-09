f5fcbdc3d5fca5ccd4a6d92b4c7fd4793b93cbb2
Xharles <charlesnzubechukwu000@gmail.com>
2022-04-09 13:49:43 +0100

    add proposals endpoints

--- index.js
+++ index.js
@@ -1,52 +1,150 @@
 // Add Express
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose"); // import mongoose
 const { Mint } = require("./scripts/mint-nft");
+const Proposal = require("./models/proposal");
+
+const { MONGO_URL } = process.env;
 
 // Initialize Express
 const app = express();
+const router = express.Router();
+mongoose.connect(MONGO_URL, {}, (err) => {
+  if (err) console.log(err);
+  else console.log("mongdb is connected");
+});
 
-// Use this soon
-// var allowedOrigins = ["http://localhost:3000", "http://yourapp.com"];
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // allow requests with no origin
-//       // (like mobile apps or curl requests)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         var msg =
-//           "The CORS policy for this site does not " +
-//           "allow access from the specified Origin.";
-//         return callback(new Error(msg), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
-
-
-app.use(cors())
+app.use(cors());
 
 //Enable prefligt
-app.options('*', cors()) // include before other routes
+app.options("*", cors()); // include before other routes
 
 // Parse JSON bodies for this app. Make sure you put
 // `app.use(express.json())` **before** your route handlers!
 app.use(express.json());
+app.use(router);
 
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
-// Create POST request
-app.post("/mint", async function requestHandler(req, res) {
-  res.setHeader("Content-Type", "application/json");
-  if (req?.body?.address) {
-    const result = await Mint(req?.body?.address);
-    res.status(200).send(JSON.stringify({ message: "Sucessful", result }));
-  } else res.status(401).send({ error: "No address sent" });
+// // Create POST request
+// app.post("/mint", async function requestHandler(req, res) {
+//   res.setHeader("Content-Type", "application/json");
+//   if (req?.body?.address) {
+//     const result = await Mint(req?.body?.address);
+//     res.status(200).send(JSON.stringify({ message: "Sucessful", result }));
+//   } else res.status(401).send({ error: "No address sent" });
+// });
+
+// Get proposals
+router.get("/proposals", async (req, res) => {
+  try {
+    let data = await Proposal.find();
+    res.status(200).json({
+      status: 200,
+      data: data,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
+});
+
+// Create a proposal
+router.post("/proposal", async (req, res) => {
+  try {
+    let proposal = new Proposal(req.body);
+    data = await proposal.save();
+    res.status(200).json({
+      status: 200,
+      data: data,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
+});
+
+// Read a proposal
+router.get("/proposal/:proposalId", async (req, res) => {
+  try {
+    let proposal = await Proposal.findOne({
+      _id: req.params.proposalId,
+    });
+    if (proposal) {
+      res.status(200).json({
+        status: 200,
+        data: proposal,
+      });
+    } else
+      res.status(400).json({
+        status: 400,
+        message: "No proposal found",
+      });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
+});
+
+// Update a proposal
+router.put("/proposal/:proposalId", async (req, res) => {
+  try {
+    let data = await Proposal.findByIdAndUpdate(
+      req.params.proposalId,
+      req.body,
+      {
+        new: true,
+      }
+    );
+    if (data) {
+      res.status(200).json({
+        status: 200,
+        data: data,
+      });
+    } else
+      res.status(400).json({
+        status: 400,
+        message: "No proposal found",
+      });
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
+});
+
+// Delete proposal
+router.delete("/proposal/:proposalId", async (req, res) => {
+  try {
+    let data = await Proposal.findByIdAndRemove(req.params.proposalId);
+    if (data) {
+      res.status(200).json({
+        status: 200,
+        message: "Proposal deleted successfully",
+      });
+    } else {
+      res.status(400).json({
+        status: 400,
+        message: "No proposal found",
+      });
+    }
+  } catch (err) {
+    res.status(400).json({
+      status: 400,
+      message: err.message,
+    });
+  }
 });
 
 // Initialize server
